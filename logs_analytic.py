304fa529b915d1cbcbd88c79cc180eed1c5dd740
Pertsev Sergey <pertsev.sergey59@yandex.ru>
2025-04-12 21:47:07 +0500

    Added console_parser module -parse args -check paths exist

--- logs_analytic.py
+++ logs_analytic.py
@@ -1,12 +1,4 @@
-import argparse
-
-
-def parse_args() -> argparse.Namespace:
-    """Function to parse arguments"""
-    parser = argparse.ArgumentParser(description="Logs Analytic utils")
-    parser.add_argument('log_paths', nargs='+', help="List of logs paths")
-    parser.add_argument('--report', help="Report name")
-    return parser.parse_args()
+from core.console_parser import parse_args
 
 
 def main() -> None:
