8ac554c86c1db04b6fa6a2e41b12dc73f3f3c5e4
Pertsev Sergey <pertsev.sergey59@yandex.ru>
2025-04-12 22:01:03 +0500

    Added doc

--- logs_analytic.py
+++ logs_analytic.py
@@ -2,6 +2,8 @@ from core.console_parser import parse_args
 
 
 def main() -> None:
+    """Entry point of the program"""
+    # get arguments from command line
     args = parse_args()
     print(args)
 
