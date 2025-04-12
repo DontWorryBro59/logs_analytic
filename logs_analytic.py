062613bbbf356935c848bad3e72cd79666182d2b
Pertsev Sergey <pertsev.sergey59@yandex.ru>
2025-04-12 22:10:04 +0500

    Added: report choice logic

--- logs_analytic.py
+++ logs_analytic.py
@@ -3,8 +3,11 @@ from core.console_parser import parse_args, check_paths_exist
 
 def get_report(report_name: str, paths: list[str]) -> None:
     """Function to get report"""
-    print(f"Report name: {report_name}, paths: {paths}")
-    pass
+    if report_name == "handlers":
+        # TODO: implement get_handlers_report
+        print(f"Report name: {report_name}, paths: {paths}")
+    else:
+        print(f"Report name not found, your choice is: {report_name}")
 
 
 def main() -> None:
