f5e3281339975190b721a562fec3ec7e00f31b26
Pertsev Sergey <pertsev.sergey59@yandex.ru>
2025-04-12 22:08:05 +0500

    Added: get_report function and check correct_paths

--- logs_analytic.py
+++ logs_analytic.py
@@ -1,13 +1,23 @@
 from core.console_parser import parse_args, check_paths_exist
 
 
+def get_report(report_name: str, paths: list[str]) -> None:
+    """Function to get report"""
+    print(f"Report name: {report_name}, paths: {paths}")
+    pass
+
+
 def main() -> None:
     """Entry point of the program"""
     # get arguments from command line
     args = parse_args()
     # check if paths exist
     correct_paths = check_paths_exist(args.log_paths)
-
+    if not correct_paths:
+        print("[Error!]: no correct paths !")
+        return
+    # get report
+    get_report(args.report, correct_paths)
 
 
 if __name__ == '__main__':
