e1d6f5a499680fba4b7cae135332ae42b0fdeaa1
Pertsev Sergey <pertsev.sergey59@yandex.ru>
2025-04-12 21:40:54 +0500

    Added parese_args (get log_paths and report name)

--- logs_analytic.py
+++ logs_analytic.py
@@ -1,5 +1,17 @@
+import argparse
+
+
+def parse_args() -> argparse.Namespace:
+    """Function to parse arguments"""
+    parser = argparse.ArgumentParser(description="Logs Analytic utils")
+    parser.add_argument('log_paths', nargs='+', help="List of logs paths")
+    parser.add_argument('--report', help="Report name")
+    return parser.parse_args()
+
+
 def main() -> None:
-    pass
+    args = parse_args()
+    print(args)
 
 
 if __name__ == '__main__':
