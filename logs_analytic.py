fd61002b7075b5c5d7df9fb8b38a680e86dd2ce0
Pertsev Sergey <pertsev.sergey59@yandex.ru>
2025-04-12 22:04:26 +0500

    Added check correct paths

--- logs_analytic.py
+++ logs_analytic.py
@@ -1,11 +1,13 @@
-from core.console_parser import parse_args
+from core.console_parser import parse_args, check_paths_exist
 
 
 def main() -> None:
     """Entry point of the program"""
     # get arguments from command line
     args = parse_args()
-    print(args)
+    # check if paths exist
+    correct_paths = check_paths_exist(args.log_paths)
+
 
 
 if __name__ == '__main__':
