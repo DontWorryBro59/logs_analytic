7ea6100d0f08309a2f7cbae48066eebe602604bd
Pertsev Sergey <pertsev.sergey59@yandex.ru>
2025-04-13 15:10:14 +0500

    Added: func and property for atr

--- reports/handlers/handler_conf.py
+++ reports/handlers/handler_conf.py
@@ -2,8 +2,16 @@ import re
 
 
 class HandlerConf:
-    all_levels = ['DEBUG', 'INFO', 'WARNING', 'ERROR', 'CRITICAL']
-    handler_pattern = re.compile(
-        r'^\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2},\d{3} '
-        r'(?P<level>DEBUG|INFO|WARNING|ERROR|CRITICAL) .*?: .*?(?P<path>/[^\s\[]+)'
-    )
+
+    @property
+    def all_levels(self) -> list[str]:
+        all_levels = ['DEBUG', 'INFO', 'WARNING', 'ERROR', 'CRITICAL']
+        return all_levels
+
+    @property
+    def handler_pattern(self) -> re.Pattern:
+        handler_pattern = re.compile(
+            r'^\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2},\d{3} '
+            r'(?P<level>DEBUG|INFO|WARNING|ERROR|CRITICAL) .*?: .*?(?P<path>/[^\s\[]+)'
+        )
+        return handler_pattern
